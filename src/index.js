2b655e6fd7d2841b0a1184addf5979aedf807afc
gopla <yudhistn@gmail.com>
2021-03-28 11:53:47 +0700

    Changes : - Cron every 30 min

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const { connectDB } = require('./utils/db')
 const { scrapTribun } = require('./scraper/tribun.scraper')
 const { scrapOkezone } = require('./scraper/okzeone.scraper')
 const { scrapDetik } = require('./scraper/detik.scraper')
+const cron = require('node-cron')
 
 connectDB()
 	.then(async () => {
 		console.log(` -> Database connected!`)
-		await scrapTribun(['news', 'techno', 'seleb'])
-		await scrapOkezone(['travel', 'news', 'celebrity'])
-		await scrapDetik(['travel', 'health', 'sport'])
+		cron.schedule('*/30 * * * *', async () => {
+			await scrapTribun(['news', 'techno', 'seleb'])
+			await scrapOkezone(['travel', 'news', 'celebrity'])
+			await scrapDetik(['travel', 'health', 'sport'])
+		})
 	})
 	.catch((err) => console.log(err))
